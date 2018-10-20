3022a743ff2c9c4c74f009c16fdf51fa93324dbd
Rus Hughes <russell.hughes@gmail.com>
2018-10-20 15:11:09 +0900

    s3 l34

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -15,4 +18,18 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != "King of Clubs" {
 		t.Errorf("Expected last card of King of Clubs, but got %v", d[0])
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting.cards")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting.cards")
+
+	loadedDeck := newDeckFromFile("_decktesting.cards")
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected deck length of 52, but got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting.cards")
 }
